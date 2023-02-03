204d6ee52ddbe051ee1f1bf04ea44661b8feee07
Schiavano1 <davideschiavano22@gmail.com>
2023-02-03 17:25:14 +0100

    versione 0

---
+++ prova/src/test.java
@@ -0,0 +1,26 @@
+/**
+ * <U>classe di gervasi lo gnomo</U>
+ * @author <Mark> davide </Mark>
+ *
+ */
+public class test {
+private String b;
+
+	public test(String b) {
+
+	this.b = b;
+}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+
+	public static void main(String[] args) {
+	System.out.println("gervasi");
+	}
+
+}
